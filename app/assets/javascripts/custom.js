a14179df65e4056b55defee7b6e13147fa16b991
itw-smart <tobias@demokratie.today>
2021-01-09 16:00:59 +0100

    f5

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,14 @@
+//= require custom/mjAccordion.js
+//= require turbolinks
+
+
+var mj;
+mj = function() {
+  $(".mj_accordion").mjAccordion()
+};
+
+$(function() {
+  
+  $(document).ready(mj);
+  $(document).on('page:load', mj);
+});

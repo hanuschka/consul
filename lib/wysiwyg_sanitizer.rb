be45f3818dd6d10a6e417281ba5e84f38165b543
itw-smart <tobias@demokratie.today>
2021-01-09 15:27:13 +0100

    f4

---
+++ lib/wysiwyg_sanitizer.rb
@@ -0,0 +1,13 @@
+class WYSIWYGSanitizer
+  def allowed_tags
+    %w[p ul ol li strong em u s a h2 h3 div]
+  end
+
+  def allowed_attributes
+    %w[href class id target onclick]
+  end
+
+  def sanitize(html)
+    ActionController::Base.helpers.sanitize(html, tags: allowed_tags, attributes: allowed_attributes)
+  end
+end

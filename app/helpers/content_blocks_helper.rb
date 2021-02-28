56f4f15b4da5ac8a1b7d31a1d2bbf4749c59f63f
kiosan <sasha@vertalab.com>
2021-02-28 22:12:06 +0200

    moved helper to custom

--- app/helpers/content_blocks_helper.rb
+++ app/helpers/content_blocks_helper.rb
@@ -8,14 +8,4 @@ module ContentBlocksHelper
     end
     options
   end
-
-  def render_custom_block(key)
-    block = SiteCustomization::ContentBlock.custom_block_for(key, I18n.locale)
-    if current_user.administrator?
-      edit_link = link_to t("admin.actions.edit"), edit_admin_site_customization_content_block_path(block, return_to: request.path )
-    end
-    res = block&.body || ""
-    res << edit_link ? "<br>#{edit_link}" : ""
-    raw res
-  end
 end

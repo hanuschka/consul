23454570bfa2fcc4a6f2a22c3aca2fdf82669af8
kiosan <sasha@vertalab.com>
2021-03-13 21:19:47 +0200

    added sms verification process

--- app/models/custom/proposal.rb
+++ app/models/custom/proposal.rb
@@ -1,6 +1,6 @@
 require_dependency Rails.root.join("app", "models", "proposal").to_s
 class Proposal < ApplicationRecord
-
+  _validators.reject! { |attribute, _| attribute == :responsible_name }
   def project_name
     tags.project.first&.name
   end

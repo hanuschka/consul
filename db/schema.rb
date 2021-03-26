44b25c2f21fb97906ac5d60734b06c53751079c2
Mike <michael.svystun@gmail.com>
2021-03-26 14:13:30 +0200

    Setup project (#60)
    * Setup project (#16)
    
    custom tags merge
    
    * Setup project (#56)
    
    * 163, 187, 177
    
    * minor change
    
    * con-190
    
    * con-156
    
    Co-authored-by: kiosan <sasha@vertalab.com>
    
    * changes to ES5
    
    Co-authored-by: Oleksandr Bondar <obondar@gmail.com>
    Co-authored-by: itw-smart <66314520+itw-smart@users.noreply.github.com>
    Co-authored-by: kiosan <sasha@vertalab.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210319081657) do
+ActiveRecord::Schema.define(version: 20210326072836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1223,6 +1223,7 @@ ActiveRecord::Schema.define(version: 20210319081657) do
     t.boolean "proposal_phase_active"
     t.date "proposal_phase_start"
     t.date "proposal_phase_end"
+    t.boolean "show_in_navigation"
     t.index ["parent_id"], name: "index_projekts_on_parent_id"
   end
 

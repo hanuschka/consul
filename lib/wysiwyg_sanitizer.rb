da5a1514f48af343701b25bdd5119f4f920ff26e
Mike Svystun <michael.svystun@gmail.com>
2021-04-23 15:16:17 +0300

    ads iframe

--- lib/wysiwyg_sanitizer.rb
+++ lib/wysiwyg_sanitizer.rb
@@ -1,10 +1,10 @@
 class WYSIWYGSanitizer
   def allowed_tags
-    %w[p ul ol li strong em u s a h2 h3]
+    %w[p ul ol li strong em u s a h2 h3 iframe]
   end
 
   def allowed_attributes
-    %w[href]
+    %w[href width height]
   end
 
   def sanitize(html)

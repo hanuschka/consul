0019d7ba43acd8a7bf7a9021920dfb4a4ba333e7
Mike Svystun <michael.svystun@gmail.com>
2021-06-18 16:43:13 +0300

    sync

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :deploy do
 
   after :published, "deploy:restart"
   before "deploy:restart", "puma:restart"
-  before "deploy:restart", "sudo systemctl status delayed_job2"
+  before "deploy:restart", "restart_delayed_jobs"
   before "deploy:restart", "puma:start"
 
   after :finished, "refresh_sitemap"
@@ -104,6 +104,16 @@ task :execute_release_tasks do
     within release_path do
       with rails_env: fetch(:rails_env) do
         execute :rake, "consul:execute_release_tasks"
+      end
+    end
+  end
+end
+
+task :restart_delayed_jobs do
+  on roles(:app) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute "sudo systemctl status delayed_job2"
       end
     end
   end

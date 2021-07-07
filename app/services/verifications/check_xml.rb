e700994a7f248e2532149ea1859d2b0ea6bee2e6
Mike Svystun <michael.svystun@gmail.com>
2021-07-07 15:07:40 +0300

    con-447

--- app/services/verifications/check_xml.rb
+++ app/services/verifications/check_xml.rb
@@ -1,6 +1,6 @@
 module Verifications
   class CheckXML
-    def self.check_verification_request_sucess
+    def self.check_verification_request
       success_responce = "/home/deploy/consul/validation/21070212202033_1234_AN.xml"
       failed_responce = "/home/deploy/consul/validation/21070212302033_2345_AN.xml"
       no_responce = "/home/deploy/consul/validation/2107021230203312_2345_AN.xml"
@@ -19,26 +19,5 @@ module Verifications
       end
       file.close if file
     end
-
-    def self.check_verification_request_fail
-      success_responce = "/home/deploy/consul/validation/21070212202033_1234_AN.xml"
-      failed_responce = "/home/deploy/consul/validation/21070212302033_2345_AN.xml"
-      no_responce = "/home/deploy/consul/validation/2107021230203312_2345_AN.xml"
-
-      # success_responce = "/home/mike/verifications/21070212202033_1234_AN.xml"
-      # failed_responce = "/home/mike/verifications/21070212302033_2345_AN.xml"
-      # no_responce = "/home/mike/verifications/2107021230203312_2345_AN.xml"
-
-      file = File.open(no_responce)
-      doc = Nokogiri::XML(file)
-      response = doc.at_xpath('request').at_xpath('kombi').text
-      if response == "true"
-        User.first.touch
-      elsif response == 'false'
-        User.first.touch
-      end
-      file.close if file
-    end
-
   end
 end

e1d0d964efecf92de6f8a21f9275fb6825defad1
Mike Svystun <michael.svystun@gmail.com>
2021-08-17 18:30:53 +0300

    xml bug

--- app/jobs/check_user_verification_request_job.rb
+++ app/jobs/check_user_verification_request_job.rb
@@ -1,8 +1,5 @@
 class CheckUserVerificationRequestJob < ApplicationJob
-  queue_as :default
-  retry_on Errno::ENOENT, wait: 4.seconds, attempts: 10, queue: :default, priority: 0
-
   def perform(file)
-    Verifications:: CheckXML.check_verification_request(file)
+    Verifications::CheckXML.check_verification_request(file)
   end
 end

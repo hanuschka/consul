5ae3738e265449301d2ab0c47abd037e765f65a6
Mike Svystun <michael.svystun@gmail.com>
2021-08-18 11:29:17 +0300

    bug with voting

--- app/models/custom/poll.rb
+++ app/models/custom/poll.rb
@@ -13,7 +13,7 @@ class Poll < ApplicationRecord
       !user.organization? &&
       user.level_three_verified? &&
       current? &&
-      (!geozone_restricted || geozone_ids.include?(user.geozone_id) || user.level_three_verified?)
+      (!geozone_restricted || (geozone_restricted && geozone_ids.include?(user.geozone_id)))
   end
 
   def comments_allowed?(user)

4e67ec8984117562480e4a3d12c9ffe256a92aed
Mike Svystun <michael.svystun@gmail.com>
2021-10-21 11:18:25 +0300

    con-480

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -11,6 +11,10 @@ CKEDITOR.editorConfig = function( config )
   config.filebrowserImageUploadUrl = "/ckeditor/pictures?";
   config.filebrowserUploadMethod = "form";
 
+  config.font_names =
+    'MyriadPro;' +
+    'DMSerifDisplay';
+
   config.allowedContent = true;
   config.format_tags = "p;h1;h2;h3;h4;h5;h6";
 

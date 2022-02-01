3e99fe3150ad4ac08f9b76ab7e17ce11991351a4
Mike Svystun <michael.svystun@gmail.com>
2022-02-01 14:01:49 +0200

    con-889

--- db/content_blocks/deficiency_reports_help.rb
+++ db/content_blocks/deficiency_reports_help.rb
@@ -1,9 +1,9 @@
 def deficiency_reports_help_content
   content = '
     <p class="lead">
-      <strong>Hilfe zu Mängelmeldungen</strong>
+      <strong>Hilfe zu Anliegen</strong>
     </p>
-    <p>Mängel können, abhängig vom eingestellten Verifizierungsverfahren, von jeder Benutzerin und jedem Benutzer eingebracht werden. Sie bieten eine einfache Möglichkeit auf Mängel in der Stadt hinzuweisen. Wir danken für Ihre Unterstützung!</p>
+    <p>Anliegen können, abhängig vom eingestellten Verifizierungsverfahren, von jeder Benutzerin und jedem Benutzer eingebracht werden. Sie bieten eine einfache Möglichkeit auf Dinge in der Stadt hinzuweisen. Wir danken für Ihre Unterstützung!</p>
   '
 end
 

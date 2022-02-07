813bfb491737122d762292f3fc2b8f34e8080d18
Mike Svystun <michael.svystun@gmail.com>
2022-02-07 11:32:03 +0200

    con-888

--- config/routes.rb
+++ config/routes.rb
@@ -56,9 +56,10 @@ Rails.application.routes.draw do
   end
 
   # More info pages
-  get "help",             to: "pages#show", id: "help/index",             as: "help"
-  get "help/how-to-use",  to: "pages#show", id: "help/how_to_use/index",  as: "how_to_use"
-  get "help/faq",         to: "pages#show", id: "faq",                    as: "faq"
+  get "help",             to: "pages#show",        id: "help/index",             as: "help"
+  get "newsletter",       to: "pages#newsletter",                                as: "newsletter"
+  get "help/how-to-use",  to: "pages#show",        id: "help/how_to_use/index",  as: "how_to_use"
+  get "help/faq",         to: "pages#show",        id: "faq",                    as: "faq"
 
   # Static pages
   resources :pages, path: "/", only: [:show]

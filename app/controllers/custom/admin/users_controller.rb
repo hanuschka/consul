2fcb986248680799b60b70b5b7a05839dec16010
Mike Svystun <michael.svystun@gmail.com>
2022-10-03 22:01:54 +0300

    con-1340

--- app/controllers/custom/admin/users_controller.rb
+++ app/controllers/custom/admin/users_controller.rb
@@ -4,5 +4,6 @@ class Admin::UsersController < Admin::BaseController
   def verify
     @user = User.find(params[:id])
     @user.update!(verified_at: Time.current)
+    Mailer.manual_verification_confirmation(@user).deliver_later
   end
 end

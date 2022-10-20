433cc607918db71ff8b9bc406c9dc5f8b558cee1
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-10-20 20:14:25 +0300

    con-1345

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -119,6 +119,7 @@
 //= require_tree ./sdg_management
 //= require custom
 //= require_tree ./custom
+//= require_tree ./custom/util
 
 var initialize_modules = function() {
   "use strict";
@@ -196,6 +197,7 @@ var initialize_modules = function() {
   App.ProjektQuestionCustom.initialize();
   App.LivesubmitCheckboxCustom.initialize();
   App.OrbitInPopupFixCustom.initialize();
+  App.StikyTopbar.initialize();
 
   if ($("#projekt-selector-fields").length) {
     App.ProjektSelector.initialize();

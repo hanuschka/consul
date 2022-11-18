b50c3f1e37f69b9a8d6c4dcfcbabbb4924be7340
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-11-18 16:24:53 +0200

    Update

--- app/components/custom/proposals/proposal_list_item/component.rb
+++ app/components/custom/proposals/proposal_list_item/component.rb
@@ -9,7 +9,7 @@ class Proposals::ProposalListItem::Component < ApplicationComponent
   def component_attributes
     {
       resource: @proposal,
-      head_title: proposal.projekt.page.title,
+      projekt: proposal.projekt,
       title: proposal.title,
       description: proposal.summary,
       tags: proposal.tags.first(3),
@@ -18,7 +18,7 @@ class Proposals::ProposalListItem::Component < ApplicationComponent
       # end_date: proposal.total_duration_end,
       wide: @wide,
       resource_name: "Proposal",
-      url: helpers.proposals_path(proposal),
+      url: helpers.proposal_path(proposal),
       image_url: proposal.image&.variant(:medium),
       date: proposal.created_at,
       author: proposal.author,
--- app/controllers/custom/welcome_controller.rb
+++ app/controllers/custom/welcome_controller.rb
@@ -20,8 +20,8 @@ class WelcomeController < ApplicationController
     # TODO
     # @active_projekts = @active_feeds.include?("active_projekts") ? @feeds.find{ |feed| feed.kind == 'active_projekts' }.active_projekts : []
     @active_projekts = Projekt.current.first(3)
-    @proposals = Proposal.first(3)
-    @debates = Debate.first(3)
+    @proposals = Proposal.where.not(projekt_id: nil).first(3)
+    @debates = Debate.where.not(projekt_id: nil).first(3)
     @expired_projekts = @active_feeds.include?("expired_projekts") ? @feeds.find{ |feed| feed.kind == 'expired_projekts' }.expired_projekts : []
     @latest_polls = @active_feeds.include?("polls") ? @feeds.find{ |feed| feed.kind == 'polls' }.polls : []
 

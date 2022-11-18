be5663d88071341c0c8a0c56c42649e959cea749
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-11-18 17:44:31 +0200

    Update

---
+++ app/components/custom/polls/list_item/component.rb
@@ -0,0 +1,28 @@
+class Polls::ListItem::Component < ApplicationComponent
+  attr_reader :poll
+
+  def initialize(poll:, wide: false)
+    @poll = poll
+    @wide = wide
+  end
+
+  def component_attributes
+    {
+      resource: @poll,
+      projekt: poll.projekt,
+      title: poll.title,
+      description: poll.summary,
+      tags: poll.tags.first(3),
+      sdgs: poll.related_sdgs.first(5),
+      # start_date: poll.total_duration_start,
+      # end_date: poll.total_duration_end,
+      wide: @wide,
+      resource_name: "poll",
+      url: helpers.proposal_path(poll),
+      image_url: poll.image&.variant(:medium),
+      date: poll.created_at,
+      author: poll.author,
+      id: poll.id
+    }
+  end
+end

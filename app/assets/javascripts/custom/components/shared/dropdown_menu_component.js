f22c65bcfa765b4cae05470e869469b9754a06b7
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-11-29 01:05:30 +0200

    Update

--- app/assets/javascripts/custom/components/shared/dropdown_menu_component.js
+++ app/assets/javascripts/custom/components/shared/dropdown_menu_component.js
@@ -14,32 +14,18 @@
     openDropdown: function(e) {
       e.preventDefault();
 
-      var dropdownOpenButton = e.currentTarget;
+      var $dropdownElement = $(e.currentTarget.parentElement);
 
-      if (dropdownOpenButton.nextElementSibling.classList.contains("dropdown-active") === true) {
-        // Close the clicked dropdown
-        dropdownOpenButton.parentElement.classList.remove("dropdown-open");
-        dropdownOpenButton.nextElementSibling.classList.remove("dropdown-active");
+      if ($dropdownElement.hasClass("dropdown-open") === true) {
+        $dropdownElement.removeClass("dropdown-open");
       } else {
-        // Close the opend dropdown
         this.closeDropdown();
-
-        // add the open and active class(Opening the DropDown)
-        dropdownOpenButton.parentElement.classList.add("dropdown-open");
-        dropdownOpenButton.nextElementSibling.classList.add("dropdown-active");
+        $dropdownElement.addClass("dropdown-open");
       }
     },
 
     closeDropdown: function() {
-      // remove the open and active class from other opened Dropdown (Closing the opend DropDown)
-      document.querySelectorAll(".dropdown-container").forEach(function(container) {
-        container.classList.remove("dropdown-open");
-      });
-
-      document.querySelectorAll(".dropdown-menu").forEach(function(menu) {
-        menu.classList.remove("dropdown-active");
-      });
+      $(".dropdown-container.dropdown-open").removeClass("dropdown-open");
     },
-
   };
 }).call(this);

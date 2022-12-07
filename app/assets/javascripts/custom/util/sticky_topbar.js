58f81836c4c97afed7d985af0fc69dcbb78f3125
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-12-08 00:26:20 +0200

    Update

--- app/assets/javascripts/custom/util/sticky_topbar.js
+++ app/assets/javascripts/custom/util/sticky_topbar.js
@@ -1,8 +1,29 @@
 (function() {
   "use strict";
-  App.StikyTopbar = {
+  App.StikyHeader = {
+    initialized: false,
+
     initialize: function() {
-      console.log('StikyTopbar initialize')
+      if (this.initialized)  {
+        return;
+      }
+
+      window.onscroll = this.handleScroll.bind(this);
+
+      // Get the navbar
+      this.header = document.querySelector(".js-stiky-header");
+
+      // Get the offset position of the header
+      this.initialHeaderOffsetY = this.header.offsetTop;
+      this.initialized = true;
+    },
+
+    handleScroll: function() {
+      if (window.pageYOffset > this.initialHeaderOffsetY) {
+        this.header.classList.add("-sticky")
+      } else {
+        this.header.classList.remove("-sticky");
+      }
     }
   };
 }).call(this);

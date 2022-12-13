a1e5ec8c7153e2722ca40da4f2bba7c4ddea9fb5
Michael Svystun <michael.svystun@gmail.com>
2022-12-13 17:16:09 +0200

    sync

--- db/migrate/20221013092846_add_qualified_votes_count_to_budget_investment.rb
+++
@@ -1,5 +0,0 @@
-class AddQualifiedVotesCountToBudgetInvestment < ActiveRecord::Migration[5.2]
-  def change
-    add_column :budget_investments, :qualified_votes_count, :integer, default: 0
-  end
-end

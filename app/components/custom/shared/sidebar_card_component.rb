0013f64ab4ca7b217f15a31d07ba899378913376
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-01-19 10:00:16 +0200

    Update

--- app/components/custom/shared/sidebar_card_component.rb
+++ app/components/custom/shared/sidebar_card_component.rb
@@ -2,7 +2,7 @@ class Shared::SidebarCardComponent < ApplicationComponent
   renders_one :body
   renders_one :additional_body
 
-  def initialize(title:, icon_name: "info")
+  def initialize(title: nil, icon_name: "info")
     @title = title
     @icon_name = icon_name
   end

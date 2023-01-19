02dd85d51d18f6c303598338c3f906ea2b9e7db2
Michael Svystun <michael.svystun@gmail.com>
2023-01-19 13:14:55 +0200

    minor fix

--- db/migrate/20230118122638_move_data_from_bam_street_to_city_street.rb
+++
@@ -1,9 +0,0 @@
-class MoveDataFromBamStreetToCityStreet < ActiveRecord::Migration[5.2]
-  def up
-    # connect users to city streets
-    execute "UPDATE users SET city_street_id = bam_street_id"
-  end
-
-  def down
-  end
-end
--- db/migrate/20230118125010_move_data_from_bam_unique_stamp_to_unique_stamp.rb
+++
@@ -1,9 +0,0 @@
-class MoveDataFromBamUniqueStampToUniqueStamp < ActiveRecord::Migration[5.2]
-  def up
-    # connect users to city streets
-    execute "UPDATE users SET unique_stamp = LOWER(bam_unique_stamp)"
-  end
-
-  def down
-  end
-end

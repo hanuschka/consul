6cd168877795d5631c58e8064841257f9a16b87f
Michael Svystun <michael.svystun@gmail.com>
2023-01-23 11:55:27 +0200

    minor fix

--- app/models/custom/poll/booth.rb
+++
@@ -1,9 +0,0 @@
-require_dependency Rails.root.join("app", "models", "poll", "booth").to_s
-
-class Poll
-  class Booth < ApplicationRecord
-    def self.available
-      where(polls: { id: Poll.current_or_recounting }).joins(:polls).distinct
-    end
-  end
-end

21e4690d3e4c711fa01e9336893e89f406b1ea47
Michael Svystun <michael.svystun@gmail.com>
2023-01-23 19:15:02 +0200

    sync

--- db/migrate/20220627221708_create_projekt_overview_special_projekt.rb
+++ db/migrate/20220627221708_create_projekt_overview_special_projekt.rb
@@ -1,9 +1,9 @@
 class CreateProjektOverviewSpecialProjekt < ActiveRecord::Migration[5.2]
   def up
-    Projekt.find_or_create_by(
-      name: 'Overview page',
-      special_name: 'projekt_overview_page',
-      special: true
-    )
+    # Projekt.find_or_create_by(
+    #   name: 'Overview page',
+    #   special_name: 'projekt_overview_page',
+    #   special: true
+    # )
   end
 end

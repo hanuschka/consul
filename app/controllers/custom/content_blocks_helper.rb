c2e21e3a0ef42a96e8957b3f576d77b672c549fa
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-02-24 17:58:55 +0200

    Update

--- app/controllers/custom/content_blocks_helper.rb
+++ app/controllers/custom/content_blocks_helper.rb
@@ -4,7 +4,7 @@ module ContentBlocksHelper
 
   def render_custom_block(key, default_content: nil)
     block = SiteCustomization::ContentBlock.custom_block_for(key, I18n.locale)
-    block_body = block&.body || default_content || ""
+    block_body = block&.body.presence || default_content || ""
 
     if current_user && current_user.administrator?
       edit_link = link_to('<i class="fas fa-edit"></i>'.html_safe, edit_admin_site_customization_content_block_path(block, return_to: request.path) )

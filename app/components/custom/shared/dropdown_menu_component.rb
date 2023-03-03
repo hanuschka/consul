b55fc3a0110f3b0dc198b31dddd65b5f35503210
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2023-03-03 16:43:15 +0200

    Update

--- app/components/custom/shared/dropdown_menu_component.rb
+++ app/components/custom/shared/dropdown_menu_component.rb
@@ -1,6 +1,8 @@
 class Shared::DropdownMenuComponent < ApplicationComponent
   renders_many :options
 
+  attr_reader :selected_option
+
   def initialize(name: nil, item_css_class: nil, selected_option: nil)
     @name = name
     @item_css_class = item_css_class

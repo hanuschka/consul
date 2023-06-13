bee202a48a2098af195e48a8577ebbdeae2da097
Michael Svystun <michael.svystun@gmail.com>
2023-06-13 14:57:18 +0300

    con-1559

---
+++ app/assets/javascripts/cli/matomo.js
@@ -0,0 +1,25 @@
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+
+(function() {
+  var u="https://unsereheimatgestalten.matomo.cloud/";
+  var previousPageUrl = null;
+
+  addEventListener('turbolinks:load', function(event) {
+    if (previousPageUrl) {
+      _paq.push(['setReferrerUrl', previousPageUrl]);
+      _paq.push(['setCustomUrl', window.location.href]);
+      _paq.push(['setDocumentTitle', document.title]);
+      _paq.push(['trackPageView']);
+    }
+
+    previousPageUrl = window.location.href;
+  });
+
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src='//cdn.matomo.cloud/unsereheimatgestalten.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+})();

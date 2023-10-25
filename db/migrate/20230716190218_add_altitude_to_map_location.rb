36d8e4e9664d46a3e227822dbe1e42930d6cc216
Michael Svystun <michael.svystun@gmail.com>
2023-10-25 18:21:32 +0300

    sync

--- db/migrate/20230716190218_add_altitude_to_map_location.rb
+++
@@ -1,5 +0,0 @@
-class AddAltitudeToMapLocation < ActiveRecord::Migration[5.2]
-  def change
-    add_column :map_locations, :altitude, :float unless column_exists? :map_locations, :altitude
-  end
-end

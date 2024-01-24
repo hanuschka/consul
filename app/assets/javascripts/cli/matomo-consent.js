45bf73958dccff1adf1b55908bef1a345f8857da
Michael Svystun <michael.svystun@gmail.com>
2024-01-24 14:33:04 +0000

    minor fix

--- app/assets/javascripts/cli/matomo-consent.js
+++ app/assets/javascripts/cli/matomo-consent.js
@@ -1,7 +1,5 @@
-$(document).on('turbolinks:load', function() {
-  if (window.location.pathname == "/matomo-consent") {
-    var matomoExternalScript = document.createElement('script');
-    matomoExternalScript.src = 'https://demodemokratietoday.matomo.cloud/index.php?module=CoreAdminHome&action=optOutJS&divId=matomo-opt-out&language=auto&showIntro=1';
-    document.head.appendChild(matomoExternalScript);
-  }
-});
+if (window.location.pathname == "/matomo-consent") {
+  var matomoExternalScript = document.createElement('script');
+  matomoExternalScript.src = 'https://demodemokratietoday.matomo.cloud/index.php?module=CoreAdminHome&action=optOutJS&divId=matomo-opt-out&language=auto&showIntro=1';
+  document.head.appendChild(matomoExternalScript);
+}

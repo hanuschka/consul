c23458b6039df717b3272e666481b6aba90f18cc
Michael Svystun <michael.svystun@gmail.com>
2024-03-22 16:40:31 +0000

    sync

--- app/helpers/projekt_questions_helper.rb
+++ app/helpers/projekt_questions_helper.rb
@@ -2,22 +2,6 @@ module ProjektQuestionsHelper
   def cannot_answer_projekt_question_callout_text(permission_problem_key, projekt_phase)
     return nil if permission_problem_key.blank?
 
-<<<<<<< HEAD
-    if permission_problem_key == :not_logged_in
-      sanitize(t("custom.projekt_phases.permission_problem.#{projekt_phase.resources_name}.#{permission_problem_key}",
-               sign_in: link_to_signin, sign_up: link_to_signup))
-
-    else
-      sanitize(t("custom.projekt_phases.permission_problem.#{projekt_phase.resources_name}.#{permission_problem_key}",
-               verify: link_to_verify_account,
-               city: Setting["org_name"],
-               geozones: projekt_phase.geozone_restrictions_formatted,
-               age_restriction: projekt_phase.age_restriction_formatted,
-               restricted_streets: projekt_phase.street_restrictions_formatted,
-               individual_group_values: projekt_phase.individual_group_value_restriction_formatted
-              ), attributes: %w(class rel data-method href))
-    end
-=======
     sanitize(t("custom.projekt_phases.permission_problem.#{projekt_phase.resources_name}.#{permission_problem_key}",
              sign_in: link_to_signin, sign_up: link_to_signup,
              guest_sign_in: link_to_guest_signin,
@@ -28,6 +12,5 @@ module ProjektQuestionsHelper
              restricted_streets: projekt_phase.street_restrictions_formatted,
              individual_group_values: projekt_phase.individual_group_value_restriction_formatted
             ))
->>>>>>> e1e13ab92341decb485954fe7961efba925d1a94
   end
 end

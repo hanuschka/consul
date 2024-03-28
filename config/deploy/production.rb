1b6ffd591bcf5d2b15c8ae6d38752592c0dfc7fb
Michael Svystun <michael.svystun@gmail.com>
2024-03-28 12:59:08 +0000

    minor fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,8 @@ server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db i
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
 #server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
 #server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
+
+set :default_env, {
+  "XDG_RUNTIME_DIR" => "/run/user/1001",
+  "DBUS_SESSION_BUS_ADDRESS" => "unix:path=/run/user/1001/bus"
+}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,8 @@
 set :branch, ENV["branch"]
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
+
+set :default_env, {
+  "XDG_RUNTIME_DIR" => "/run/user/1001",
+  "DBUS_SESSION_BUS_ADDRESS" => "unix:path=/run/user/1001/bus"
+}

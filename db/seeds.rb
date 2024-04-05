3512ede4bfc489d0e6be1ea3e01f7343312e7230
Michael Svystun <michael.svystun@gmail.com>
2024-04-05 13:57:44 +0100

    minor fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 if Administrator.count == 0 && !Rails.env.test?
   admin = User.create!(username: "admin", email: "admin@consul.dev", password: "12345678",
                        password_confirmation: "12345678", confirmed_at: Time.current,
-                       terms_of_service: "1")
+                       terms_data_storage: "1", terms_data_protection: "1", terms_general: "1")
   admin.create_administrator
 end
 

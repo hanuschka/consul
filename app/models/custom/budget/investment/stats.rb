e9325162b0bdfb8ff6f26350b180a818c1d9d524
Michael Svystun <michael.svystun@gmail.com>
2024-06-11 10:09:23 +0100

    con-2059

--- app/models/custom/budget/investment/stats.rb
+++ app/models/custom/budget/investment/stats.rb
@@ -24,11 +24,13 @@ class Budget::Investment::Stats < Budget::Stats
     end
 
     def balloters # changed
-      @balloters ||= investment.budget_ballot_lines.joins(:ballot).pluck("budget_ballots.user_id").compact
+      # @balloters ||= investment.budget_ballot_lines.joins(:ballot).pluck("budget_ballots.user_id").compact
+      @balloters ||= investment.budget_ballot_lines.joins(ballot: :user).where.not(users: { verified_at: nil, geozone_id: nil }).pluck("budget_ballots.user_id").compact
     end
 
     def poll_ballot_voters
-      @poll_ballot_voters ||= investment.budget.poll ? investment.budget.poll.voters.pluck(:user_id) : []
+      # @poll_ballot_voters ||= investment.budget.poll ? investment.budget.poll.voters.pluck(:user_id) : []
+      @poll_ballot_voters ||= investment.budget.poll ? investment.budget.poll.voters.joins(:user).where.not(users: { verified_at: nil, geozone_id: nil }).pluck(:user_id) : [] #CON-2085
     end
 
     def vote_phase_enabled? # changed

3244c8c52bf066179c1af6ce7050e91f368b0745
Michael Svystun <michael.svystun@gmail.com>
2024-06-24 11:11:08 +0100

    minor fix

--- db/migrate/20240624093425_add_auth_image_link_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthImageLinkToUsers < ActiveRecord::Migration[6.1]
-  def change
-    unless column_exists? :users, :auth_image_link
-      add_column :users, :auth_image_link, :string
-    end
-  end
-end

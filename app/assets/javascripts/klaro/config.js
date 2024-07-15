69ca0faacbc4028bac9632a5831cf6c8cd46042e
Michael Svystun <michael.svystun@gmail.com>
2024-07-15 20:13:34 +0100

    sync

--- app/assets/javascripts/klaro/config.js
+++ app/assets/javascripts/klaro/config.js
@@ -298,27 +298,27 @@ var klaroConfig = {
         //     purposes: ['styling'],
         //     required: true,
         // },
-        // { // uncomment if translate widget is enabled
-        //   name: 'google_translate_accepted',
-        //   default: false,
-        //   purposes: ['translation'],
-        //   translations: {
-        //     en: {
-        //       title: 'Google Translate'
-        //     },
-        //     de: {
-        //       title: 'Google Übersetzer',
-        //     },
-        //   },
-        //   cookies: ['googtrans'],
-        //   callback: function(consent, service) {
-        //     if ( consent == false && document.getElementById("google_translate_element") ) {
-        //       location.reload();
-        //     } else if ( consent == true && !document.getElementById("google_translate_element") ) {
-        //       location.reload();
-        //     }
-        //   }
-        // },
+        { // uncomment if translate widget is enabled
+          name: 'google_translate_accepted',
+          default: false,
+          purposes: ['translation'],
+          translations: {
+            en: {
+              title: 'Google Translate'
+            },
+            de: {
+              title: 'Google Übersetzer',
+            },
+          },
+          cookies: ['googtrans'],
+          callback: function(consent, service) {
+            if ( consent == false && document.getElementById("google_translate_element") ) {
+              location.reload();
+            } else if ( consent == true && !document.getElementById("google_translate_element") ) {
+              location.reload();
+            }
+          }
+        },
         {
             name: 'system',
             title: 'Session cookie',

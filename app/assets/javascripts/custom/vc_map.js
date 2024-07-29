fea1af1c165419926b5197a6acedd63312843023
Michael Svystun <michael.svystun@gmail.com>
2024-07-29 08:51:00 +0100

    con-2148

--- app/assets/javascripts/custom/vc_map.js
+++ app/assets/javascripts/custom/vc_map.js
@@ -13,7 +13,8 @@
       vcsApp.maps.setTarget(element);
       // App.VCMap.loadModule(vcsApp, 'https://new.virtualcitymap.de/map.config.json');
       // App.VCMap.loadModule(vcsApp, 'https://demo.virtualcitymap.de/consul-kempten/config/kempten.config.json');
-      App.VCMap.loadModule(vcsApp, '/vcmap/kempten1.config.json');
+      // App.VCMap.loadModule(vcsApp, '/vcmap/kempten1.config.json');
+      App.VCMap.loadModule(vcsApp, '/vcmap/kempten2.config.json');
 
       // custom map options
       vcsApp.customMapOptions = {}

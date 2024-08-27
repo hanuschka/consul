dc8932aeae6cfcb06acf68fbfbf34a0a9fee85a1
Michael Svystun <michael.svystun@gmail.com>
2024-08-27 14:23:23 +0100

    CON-2160b

--- app/models/custom/projekt_phase_setting.rb
+++ app/models/custom/projekt_phase_setting.rb
@@ -83,14 +83,14 @@ class ProjektPhaseSetting < ApplicationRecord
 
         "ProjektPhase::VotingPhase" => {
           base: {
-            "feature.resource.intermediate_poll_results_for_admins": "active",
-            "feature.resource.show_comments": "active",
+            "feature.resource.wizard_mode": "active",
             "feature.resource.show_on_home_page": "active",
             "feature.resource.show_on_index_page": "active",
-            "feature.resource.wizard_mode": "active",
             "feature.resource.results_enabled": "",
+            "feature.resource.intermediate_poll_results_for_admins": "active",
             "feature.resource.stats_enabled": "",
             "feature.resource.advanced_stats_enabled": "",
+            "feature.resource.show_comments": "active",
             "feature.resource.show_open_answer_author_name": ""
           }
         },

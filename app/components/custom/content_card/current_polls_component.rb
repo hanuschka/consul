2c9bc3904a3848e2b94c026e19f41e7390bde0aa
Michael Svystun <michael.svystun@gmail.com>
2024-09-02 14:13:55 +0100

    merge nc-m-1 into new-connection

--- app/components/custom/content_card/current_polls_component.rb
+++ app/components/custom/content_card/current_polls_component.rb
@@ -13,12 +13,8 @@ class ContentCard::CurrentPollsComponent < ApplicationComponent
   private
 
     def current_polls
-<<<<<<< HEAD
-      @current_polls ||= Poll.current.where(show_on_home_page: true).order(created_at: :asc).first(@limit)
-=======
       @current_polls ||= Poll.current
         .with_phase_feature("resource.show_on_home_page")
         .order(created_at: :asc)
->>>>>>> cecd52d411d1d64280c0fd7823a64b784d8684d2
     end
 end

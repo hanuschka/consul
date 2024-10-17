d9a206f20defc07401b795ad0286de83981fcad7
Michael Svystun <michael.svystun@gmail.com>
2024-10-17 15:42:08 +0100

    minor fix

--- db/migrate/20241001093348_move_only_registered_users_to_restrictions.rb
+++ db/migrate/20241001093348_move_only_registered_users_to_restrictions.rb
@@ -1,5 +1,7 @@
 class MoveOnlyRegisteredUsersToRestrictions < ActiveRecord::Migration[6.1]
   def up
+    ProjektPhase::FormularPhase.reset_column_information
+
     ProjektPhase::FormularPhase.all.find_each do |phase|
       if phase.settings.find_by(key: "feature.general.only_registered_users")&.value.present?
         phase.update!(user_status: "registered")

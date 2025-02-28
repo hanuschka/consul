fe596db31a3b61a4ca9ec976f6721c95285a27b3
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-02-28 15:06:06 +0200

    Update proposal assistant

--- app/assets/javascripts/custom/components/proposals/proposal_assistant.js
+++ app/assets/javascripts/custom/components/proposals/proposal_assistant.js
@@ -28,20 +28,62 @@
       }
     },
 
-    updateProposalTitle: function(title) {
-      const titleElement = document.querySelector(
+    highlightBanner: function() {
+     var bannerElement = document.querySelector(".user-resources-form--banner-editor")
+
+      bannerElement.classList.add("assistant-changed-field-highlight-transition")
+      bannerElement.classList.add("assistant-changed-field-highlight")
+
+      setTimeout(function() {
+        bannerElement.classList.remove("assistant-changed-field-highlight")
+      }, 3000)
+      setTimeout(function() {
+        bannerElement.classList.remove("assistant-changed-field-highlight-transition")
+      }, 3500)
+    },
+
+    updateProposal: function(params) {
+      this.updateProposalTitle(params.title, false)
+      this.updateProposalDescription(params.description, false)
+    },
+
+    updateProposalTitle: function(title, scroll) {
+      var scroll = scroll || true;
+
+      var titleElement = document.querySelector(
         ".js-user-resource-form-title"
       )
 
       titleElement.value = title
-      titleElement.scrollIntoView({block: "center", inline: "nearest"})
+
+      if (scroll) {
+        titleElement.scrollIntoView({block: "center", inline: "nearest"})
+      }
+
+      this.highlightBanner()
     },
 
-    updateProposalDescription: function(description) {
+    updateProposalDescription: function(description, scroll) {
+      var scroll = scroll || true;
+
       var editor = window.CKeditorInstancesGlobal["proposal_translations_attributes_0_description"]
       editor.setData(description)
-      editor.sourceElement.scrollIntoView({block: "center", inline: "nearest"})
-      // console.log(editor)
+
+      if (scroll) {
+        editor.sourceElement.scrollIntoView({block: "center", inline: "nearest"})
+      }
+
+      var editorContent = document.querySelector(".ck-content.ck-editor__editable")
+
+      editorContent.classList.add("assistant-changed-field-highlight-transition")
+      editorContent.classList.add("assistant-changed-field-highlight")
+
+      setTimeout(function() {
+        editorContent.classList.remove("assistant-changed-field-highlight")
+      }, 3000)
+      setTimeout(function() {
+        editorContent.classList.remove("assistant-changed-field-highlight-transition")
+      }, 3500)
     }
   };
 

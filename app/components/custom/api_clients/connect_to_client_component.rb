3100092496cf0e00f72474ca6f4112054f8d7df8
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-03-30 12:05:41 +0300

    Update connect

--- app/components/custom/api_clients/connect_to_client_component.rb
+++ app/components/custom/api_clients/connect_to_client_component.rb
@@ -2,4 +2,8 @@ class ApiClients::ConnectToClientComponent < ApplicationComponent
   def render?
     Rails.application.secrets.dt[:enabled]
   end
+
+  def show_navigate_to_dt_link?
+    ApiClient.active_dt? && current_user.on_dt?
+  end
 end

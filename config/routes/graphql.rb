9d1c3dd3d9f64d11d6ca2e65ab97ac5cff44551f
Michael Svystun <michael.svystun@gmail.com>
2025-04-02 14:40:25 +0100

    minor fix

--- config/routes/graphql.rb
+++ config/routes/graphql.rb
@@ -1,6 +1,4 @@
 post "/graphql", to: "graphql#execute"
 get "/graphql", to: "graphql#execute"
 
-if Rails.env.development?
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-end
+mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"

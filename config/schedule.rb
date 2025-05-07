f8954ec69edf28b91a52cfaf6b0673a2a3697862
Michael Svystun <michael.svystun@gmail.com>
2025-05-07 17:48:31 +0100

    minor fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,8 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
+set :output, { error: "log/cron_errors.log", standard: "log/cron.log" }
+set :job_template, "/bin/bash -lc 'export EXECJS_RUNTIME=Disabled && :job'"
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
@@ -73,7 +74,7 @@ every 1.day, at: "3:30 am", roles: [:cron] do
 end
 
 every 5.minutes do
-  rake "deficiency_reports:archive_closed", output: { error: "log/cron_error.log", standard: "log/cron.log" }
+  rake "deficiency_reports:archive_closed"
 end
 
 every 1.day, at: "6:00 am", roles: [:cron] do

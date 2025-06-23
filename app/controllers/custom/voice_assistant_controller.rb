3cb4277eeebaadf512120bd550de05c61d0056aa
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-06-23 15:48:10 +0300

    Update proposal

--- app/controllers/custom/voice_assistant_controller.rb
+++ app/controllers/custom/voice_assistant_controller.rb
@@ -3,18 +3,31 @@ class VoiceAssistantController < ActionController::Base
 
   def create_session
     dt_api = DtApi::Client.new
-    projekt_phase = ProjektPhase.find(params[:consul_projekt_phase_id])
-    projekt = projekt_phase.projekt
 
-    data = {
-      projekt: {
-        title: projekt.title,
-      },
-      projekt_phase: {
-        labels: projekt_phase.projekt_labels.as_json(only: [:id, :name]),
-        sentiments: projekt_phase.sentiments.as_json(only: [:id, :name, :color])
-      }
-    }
+    data = {}
+
+    if params[:consul_projekt_phase_id].present?
+      projekt_phase = ProjektPhase.find(params[:consul_projekt_phase_id])
+      projekt = projekt_phase.projekt
+
+      data.merge!({
+        projekt: {
+          title: projekt.title,
+        },
+        projekt_phase: {
+          labels: projekt_phase.projekt_labels.as_json(only: [:id, :name]),
+          sentiments: projekt_phase.sentiments.as_json(only: [:id, :name, :color])
+        }
+      })
+    end
+
+    if params[:codename] == "deficiency_report_voice_assistant"
+      data.merge!({
+        categories: DeficiencyReport::Category.all.as_json(only: [:id, :name])
+      })
+    end
+
+
     dt_api.voice_assistant.create_session(
       session_uuid: params[:session_uuid],
       codename: params[:codename],

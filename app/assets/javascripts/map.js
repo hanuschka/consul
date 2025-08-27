ac7272773da85ef0bd9abf761ab561ed6580da7c
Michael Svystun <michael.svystun@gmail.com>
2025-08-27 17:50:22 +0100

    minor fix

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,6 +4,23 @@
     maps: [],
     initialize: function() {
       $("*[data-map]:visible").each(function() {
+        var mapInstance = null;
+
+        for (var i = 0; i < App.Map.maps.length; i++) {
+          if (App.Map.maps[i].element.id === this.id ) {
+            mapInstance = App.Map.maps[i];
+            break;
+          }
+        }
+
+        if (mapInstance) {
+          mapInstance.map.off();
+          mapInstance.map.remove();
+          App.Map.maps = App.Map.maps.filter(function(m) {
+            return m !== mapInstance;
+          });
+        }
+
         if ( this.classList.contains("leaflet")) {
           App.Map.initializeLeafletMap(this);
         } else if ( this.classList.contains("mapbox")) {

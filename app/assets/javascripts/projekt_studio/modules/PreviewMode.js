86cd3ed1eaae781c8003731828ee57386153d0b2
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-09-10 16:32:40 +0300

    con-2516

--- app/assets/javascripts/projekt_studio/modules/PreviewMode.js
+++ app/assets/javascripts/projekt_studio/modules/PreviewMode.js
@@ -1,5 +1,3 @@
-// import { parseIframeEventData } from "consul/utils/iframeUtils";
-
 ProjektStudio.modules.PreviewMode = {
   initialized: false,
   initialize() {
@@ -58,9 +56,11 @@ ProjektStudio.modules.PreviewMode = {
 
     const sdgList = document.querySelector(".js-sidebar-card .js-sdg-goal-tag-list")
     const anyCheckedSDG = sdgList.querySelector('input:checked')
+    console.log({anyCheckedSDG})
     const sdgSidebarCard = sdgList.closest(".js-sidebar-card")
     const categoriesList = document.querySelector(".js-sidebar-card .categories--simple-selector")
     const anyCheckedCategory = categoriesList.querySelector('.js-add-tag-link.selected')
+    console.log({anyCheckedCategory})
     const categoriesSidebarCard = categoriesList.closest(".js-sidebar-card")
     const deactivatedPhases = document.querySelectorAll(".js-projekt-phase-tab.-deactivated")
 

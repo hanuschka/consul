ae9697814a365c1b51fee687ddfa479a67bcbafb
Michael Svystun <michael.svystun@gmail.com>
2025-10-10 16:52:37 +0100

    minor fix

--- app/models/custom/poll/answer.rb
+++ app/models/custom/poll/answer.rb
@@ -8,9 +8,6 @@ class Poll::Answer < ApplicationRecord
     def max_votes
       return if !question || question&.unique? || question.votation_type&.rating_scale?
 
-
-      Rails.logger.warn "Checking max votes for author #{author.id} on question #{question.id}, #{author.changed.inspect}"
-
       author.save! if author.guest? && author.changed == ["locale"]
       author.lock!
 

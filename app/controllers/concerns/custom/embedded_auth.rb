fe4c175c98b3cedbed28693bcf08e9b0e8e2c6d6
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-10-20 18:57:52 +0300

    Fix url bug

--- app/controllers/concerns/custom/embedded_auth.rb
+++ app/controllers/concerns/custom/embedded_auth.rb
@@ -81,7 +81,8 @@ module EmbeddedAuth
 
       Current.frame_current_user = user
       # binding.pry
-      request.env["warden"].set_user(user, store: false)
+      # request.env["warden"].set_user(user, store: false)
+      sign_in(user, store: false)
       # bypass_sign_in(user)
     end
 

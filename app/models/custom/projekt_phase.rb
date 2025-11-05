6cef3389c9fabcfad6f00089022beaa1cea98bc7
Michael Svystun <michael.svystun@gmail.com>
2025-11-05 10:12:37 +0000

    minor fix

--- app/models/custom/projekt_phase.rb
+++ app/models/custom/projekt_phase.rb
@@ -44,6 +44,7 @@ class ProjektPhase < ApplicationRecord
   translates :resource_form_title_placeholder, touch: true
   translates :resource_form_description_placeholder, touch: true
   translates :support_button_text, touch: true
+  translates :projekt_selector_hint # needed for globalize fallback, not used otherwise
   include Globalizable
 
   belongs_to :projekt, touch: true

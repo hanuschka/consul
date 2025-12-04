801f77b130cf4ceaa237c0cf816d3ee398ac7038
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-12-04 16:05:33 +0200

    con-2576

--- config/application.rb
+++ config/application.rb
@@ -140,10 +140,6 @@ module Consul
     config.paths["app/views"].unshift(Rails.root.join("app", "views", "custom"))
     config.paths["app/views"].unshift(Rails.root.join("app", "views", "cli"))
   end
-
-  console do
-    IRB.conf[:USE_AUTOCOMPLETE] = false
-  end
 end
 
 class Rails::Engine

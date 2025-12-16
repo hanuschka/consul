a7225cf3561dc19948c7953c411ee8c9b2b4b62b
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2025-12-16 19:56:33 +0200

    Update proxy settings

--- lib/dt_api/client.rb
+++ lib/dt_api/client.rb
@@ -3,6 +3,18 @@ class DtApi::Client
 
   base_uri "#{Dt.url}/api"
 
+  if Rails.application.secrets.web_server_proxy.present?
+    proxy_config = Rails.application.secrets.web_server_proxy
+    if proxy_config[:address].present?
+      http_proxy(
+        proxy_config[:address],
+        proxy_config[:port],
+        proxy_config[:username],
+        proxy_config[:password]
+      )
+    end
+  end
+
   def initialize(api_token = nil)
     @api_token = api_token || InternalApiClient&.dt&.service_api_token
   end

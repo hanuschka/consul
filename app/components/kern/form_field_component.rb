f841dd5ae80b5bde6a6c59a1862cab8637a1f931
Michael Svystun <michael.svystun@gmail.com>
2026-01-16 13:27:39 +0000

    con-2549

--- app/components/kern/form_field_component.rb
+++ app/components/kern/form_field_component.rb
@@ -1,8 +1,9 @@
 class Kern::FormFieldComponent < ApplicationComponent
-  def initialize(label:, hint: nil)
+  def initialize(label:, hint: nil, turbo_frame_id: nil)
     @label = label
     @hint = hint
+    @turbo_frame_id = turbo_frame_id
   end
 
-  attr_reader :label, :hint
+  attr_reader :label, :hint, :turbo_frame_id
 end

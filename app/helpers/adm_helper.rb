e6e7e0dbe9eceef9ee1ef0e9cfaea1b28a19666e
Michael Svystun <michael.svystun@gmail.com>
2026-01-30 16:46:25 +0000

    con-2549

--- app/helpers/adm_helper.rb
+++ app/helpers/adm_helper.rb
@@ -1,15 +1,6 @@
 module AdmHelper
   include Pagy::Frontend
 
-  def intro_section(title:, text:)
-    content_tag(:div, class: "kern-row") do
-      content_tag(:div, class: "kern-col-12 kern-col-md-9") do
-        content_tag(:h2, title, class: "kern-title kern-title--small") +
-          content_tag(:div, sanitize(text), class: "kern-body mb-5 mb-md-0")
-      end
-    end
-  end
-
   def restriction_label_for(projekt_phase)
     restrictions = []
     restrictions << I18n.t("adm.projekt_phases.restrictions.user_status.#{projekt_phase.user_status}") if projekt_phase.user_status.present?

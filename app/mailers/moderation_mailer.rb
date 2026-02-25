320a54a7d1b805f69242ba31f135a436e3d72a89
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-02-25 14:38:59 +0200

    con-2652

--- app/mailers/moderation_mailer.rb
+++ app/mailers/moderation_mailer.rb
@@ -3,14 +3,11 @@ class ModerationMailer < ApplicationMailer
     @flag = flag
     @flaggable = flag.flaggable
     @reporter = flag.user
-    @resource_type = @flaggable.class.model_name.human
-    @resource_snippet = resource_snippet(@flaggable)
+    @resource_title = @flaggable.respond_to?(:title) ? @flaggable.title : ""
+    @resource_url = resource_url_for(@flaggable)
     @reported_at = @flag.created_at
 
-    subject = I18n.t(
-      "custom.moderation_mailer.flag_report.subject",
-      resource_type: @resource_type
-    )
+    subject = I18n.t("custom.moderation_mailer.flag_report.subject")
     email_to = Setting["moderation.reports_notification_email"]
 
     I18n.with_locale(I18n.default_locale) do
@@ -20,10 +17,10 @@ class ModerationMailer < ApplicationMailer
 
   private
 
-    def resource_snippet(resource)
-      return "" if resource.nil?
+    def resource_url_for(resource)
+      return nil if resource.nil?
+      return proposal_url(resource) if resource.is_a?(Proposal)
 
-      snippet = resource.respond_to?(:title) ? resource.title : nil
-      snippet.presence || resource.respond_to?(:body) ? resource.body.to_s.truncate(200) : ""
+      nil
     end
 end

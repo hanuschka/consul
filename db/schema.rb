4d0a919f7b287a15ed936401ce0c00da7c6f4310
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-02-26 11:27:42 +0200

    Udpate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2026_02_23_000001) do
-=======
 ActiveRecord::Schema.define(version: 2026_02_25_000001) do
->>>>>>> new-connection
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"

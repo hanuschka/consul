55d07ee584f785704dd84285d66f8d824913b1d8
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-03-04 14:17:11 +0200

    Fix scroll test

--- app/assets/javascripts/custom/turbolinks_scroll_fix.js
+++ app/assets/javascripts/custom/turbolinks_scroll_fix.js
@@ -4,18 +4,4 @@
   if ("scrollRestoration" in history) {
     history.scrollRestoration = "manual";
   }
-
-  var isRestoreVisit = false;
-
-  document.addEventListener("turbolinks:before-visit", function(event) {
-    isRestoreVisit = event.data.action === "restore";
-  });
-
-  document.addEventListener("turbolinks:load", function() {
-    if (!isRestoreVisit) {
-      window.scrollTo(0, 0);
-    }
-
-    isRestoreVisit = false;
-  });
 }).call(this);

ca68d0e94ae8a227ec0172fa86f37bbc0485fa95
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-03-18 03:34:40 +0200

    con-2679

--- app/helpers/projekts_helper.rb
+++ app/helpers/projekts_helper.rb
@@ -1,20 +1,30 @@
 module ProjektsHelper
   def breadcrumbs_links(base_projekt, divider = '/', home_page_link = true)
-    divider_tag = content_tag(:div, divider, class: 'breadcrumbs-divider')
+    divider_tag = content_tag(:span, " #{divider} ", class: 'breadcrumbs-divider', "aria-hidden": "true")
 
-    links = base_projekt.breadcrumb_trail_ids.map do |projekt_id|
+    items = []
+
+    if home_page_link
+      items << link_to(t('custom.projekt.page.breadcrumbs.homepage'), root_path, class: 'breadcrumbs-item')
+    end
+
+    base_projekt.breadcrumb_trail_ids.each do |projekt_id|
       projekt = Projekt.find(projekt_id)
 
       if !projekt.page.published? || (projekt == base_projekt && home_page_link)
-        content_tag(:div, projekt.title, class: 'breadcrumbs-item', "aria-current": "page")
+        items << content_tag(:span, projekt.title, class: 'breadcrumbs-item', "aria-current": "page")
       else
-        link_to projekt.page.title, projekt.page.url, class: 'breadcrumbs-item'
+        items << link_to(projekt.page.title, projekt.page.url, class: 'breadcrumbs-item')
       end
     end
 
-    links.unshift(link_to t('custom.projekt.page.breadcrumbs.homepage'), root_path, class: 'breadcrumbs-item') if home_page_link
+    list_items = items.each_with_index.map do |item, index|
+      li_content = index > 0 ? (divider_tag + item) : item
+      content_tag(:li, li_content, class: 'breadcrumbs-list-item')
+    end
 
-    content_tag(:nav, safe_join(links, divider_tag).html_safe, class: 'custom-breadcrumbs', "aria-label": "Breadcrumb")
+    list = content_tag(:ol, safe_join(list_items), class: 'breadcrumbs-list')
+    content_tag(:nav, list, class: 'custom-breadcrumbs', "aria-label": "Breadcrumb")
   end
 
   def projekt_filter_resources_name

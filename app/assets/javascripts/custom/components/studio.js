e4ef43253163b38ee5b120b19b8765929d64f78d
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-03-27 12:06:59 +0200

    Fix

--- app/assets/javascripts/custom/components/studio.js
+++ app/assets/javascripts/custom/components/studio.js
@@ -86,20 +86,15 @@
 
           block.parentNode.replaceChild(wrappedElement, block);
 
-<<<<<<< HEAD
           if (wrappedElement.closest("aside, .sidebar, footer")) {
             wrappedElement.classList.add("-compact-mode");
           }
 
           $(wrappedElement).find("[data-tooltip]").foundation();
           $(wrappedElement).find("[data-orbit]").each(function() { new Foundation.Orbit($(this)); });
-        });
 
-        App.ImageGallery.initialize();
-=======
-          $(wrappedElement).find("[data-tooltip]").foundation();
+          App.ImageGallery.initialize();
         });
->>>>>>> new-connection
       }
     }
   };

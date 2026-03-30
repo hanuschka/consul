292dab684ac8db0f1b457ee50126a6f5dc1ac5f5
Tobias Weinmann <tobias@demokratie.today>
2026-03-30 18:08:15 +0200

    CON-2734

--- app/helpers/kern_helper.rb
+++ app/helpers/kern_helper.rb
@@ -34,6 +34,14 @@ module KernHelper
     end
   end
 
+  def empty_state(title:, description: nil, icon: "folder_open")
+    content_tag(:div, class: "adm-empty-state") do
+      concat(content_tag(:span, icon, class: "adm-empty-state__icon material-symbols-outlined", "aria-hidden": "true"))
+      concat(content_tag(:p, title, class: "adm-empty-state__title"))
+      concat(content_tag(:p, description, class: "adm-empty-state__description")) if description.present?
+    end
+  end
+
   def kern_alert(title:, style: :info, &block)
     content_tag(:div, class: "kern-alert kern-alert--#{style}", role: "alert") do
       header = content_tag(:div, class: "kern-alert__header") do

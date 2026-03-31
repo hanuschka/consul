960206668c0eccb98a1ac8a4753ea39a12d86bc1
Tobias Weinmann <tobias@demokratie.today>
2026-04-01 00:50:16 +0200

    fixes

--- app/controllers/adm/moderation/comments_controller.rb
+++ app/controllers/adm/moderation/comments_controller.rb
@@ -12,7 +12,7 @@ class Adm::Moderation::CommentsController < Adm::Moderation::BaseController
     @flags_count_header_options = { sort: true }
 
     @breadcrumbs = [
-      { name: I18n.t("adm.moderation.menu.title") },
+      { name: I18n.t("adm.moderation.menu.title"), icon: "comment" },
       { name: I18n.t("adm.moderation.menu.comments") }
     ]
   end
--- app/controllers/adm/moderation/proposals_controller.rb
+++ app/controllers/adm/moderation/proposals_controller.rb
@@ -12,7 +12,7 @@ class Adm::Moderation::ProposalsController < Adm::Moderation::BaseController
     @flags_count_header_options = { sort: true }
 
     @breadcrumbs = [
-      { name: I18n.t("adm.moderation.menu.title") },
+      { name: I18n.t("adm.moderation.menu.title"), icon: "article" },
       { name: I18n.t("adm.moderation.menu.proposals") }
     ]
   end

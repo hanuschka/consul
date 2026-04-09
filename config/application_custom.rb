366df962afe47dc2c16517d40e2fda140f558a18
Michael Svystun <michael.svystun@gmail.com>
2026-04-09 16:43:42 +0100

    minor fix

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,7 +2,8 @@ require_relative "../lib/middleware/downcase_route"
 
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :en
+    # config.i18n.default_locale = :en
+    config.i18n.default_locale = :de
     config.i18n.available_locales = [:en, :de]
 
     config.middleware.insert_before(0, DowncaseRoute)

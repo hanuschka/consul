7fc966fccded6b7dcdf605b7564f01a53d9e6324
Tobias <tobias@itw-smartbusiness.de>
2026-04-10 08:52:45 +0200

    CON-2789

--- app/components/adm/dashboard_home_component.rb
+++ app/components/adm/dashboard_home_component.rb
@@ -47,7 +47,18 @@ class Adm::DashboardHomeComponent < ApplicationComponent
     user_name = activity.user&.name || t(".activity_unknown_user")
     trackable_name = activity.metadata&.dig("trackable_name") || activity.trackable_type&.demodulize || "—"
     action_label = t(".activity_action_#{activity.action}", default: activity.action)
-    "#{user_name} #{t('.activity_has')} #{trackable_name} #{action_label}"
+    changed_fields = activity.metadata&.dig("changed_fields")
+    detail = if changed_fields.present?
+               " (#{changed_fields.map { |f| human_attribute_for(activity, f) }.join(', ')})"
+             else
+               ""
+             end
+    "#{user_name} #{t('.activity_has')} #{trackable_name} #{action_label}#{detail}"
+  end
+
+  def human_attribute_for(activity, field)
+    klass = activity.trackable_type&.safe_constantize
+    klass ? klass.human_attribute_name(field) : field.humanize
   end
 
   def activity_time(activity)

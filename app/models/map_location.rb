9bfbc2497bce5e39c1ca0e8f7604cb5b25a0f1ab
Michael Svystun <michael.svystun@gmail.com>
2026-04-14 11:28:51 +0100

    minor fix

--- app/models/map_location.rb
+++ app/models/map_location.rb
@@ -140,6 +140,8 @@ class MapLocation < ApplicationRecord
     geom2 =  RGeo::GeoJSON.decode(other_map_location.to_geo_json, json_parser: :json, geo_factory: factory).map(&:geometry)
 
     geom1.any? { |g1| geom2.any? { |g2| g1.intersects?(g2) } }
+  rescue RGeo::Error::InvalidGeometry
+    false
   end
 
   private

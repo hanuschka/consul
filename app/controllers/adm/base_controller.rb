2bb648517828f02b5093441ba8a27ec5adaac634
Michael Svystun <michael.svystun@gmail.com>
2026-04-16 08:26:41 +0100

    minor fix

--- app/controllers/adm/base_controller.rb
+++ app/controllers/adm/base_controller.rb
@@ -72,6 +72,8 @@ class Adm::BaseController < ActionController::Base
         Adm::NewsletterPolicy
       when "Image"
         Adm::ImagePolicy
+      when "Poll"
+        Adm::PollPolicy
       else
         raise ArgumentError, "No policy class defined for #{record_class.name}"
       end

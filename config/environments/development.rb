394dbe3afb1c59001871fd38410b28077ca42225
Michael Svystun <michael.svystun@gmail.com>
2026-04-22 12:06:02 +0100

    minor fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
   config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 

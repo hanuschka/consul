d7e73520b44c57ba6eec885fa731a9a5ab85c642
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-04-24 12:45:57 +0300

    Fix

--- app/models/navbar_item.rb
+++ app/models/navbar_item.rb
@@ -60,9 +60,13 @@ class NavbarItem < ApplicationRecord
     when "presets"
       I18n.t("navbar.presets.#{preset}")
     when "projekts"
-      projekt.title
+      projekt&.title || I18n.t("adm.navbar_items.deleted_projekt")
     when "external"
       external_title
     end
+  end
+
+  def resource_missing?
+    kind == "projekts" && projekt.blank?
   end
 end

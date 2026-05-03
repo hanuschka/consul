c3174e06eaf90bac438b9571ecbf707db69c7f39
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-05-03 04:00:49 +0300

    con-2727

--- app/components/adm/turbo_frame_placeholder_component.rb
+++ app/components/adm/turbo_frame_placeholder_component.rb
@@ -1,8 +1,10 @@
 class Adm::TurboFramePlaceholderComponent < ApplicationComponent
-  def initialize(text: nil, min_height: nil, icon: "progress_activity")
+  def initialize(text: nil, min_height: nil, icon: "progress_activity", logo_path: nil, logo_alt: "")
     @text = text
     @min_height = min_height
     @icon = icon
+    @logo_path = logo_path
+    @logo_alt = logo_alt
   end
 
   def text
@@ -11,6 +13,14 @@ class Adm::TurboFramePlaceholderComponent < ApplicationComponent
 
   def icon
     @icon
+  end
+
+  def logo_path
+    @logo_path
+  end
+
+  def logo_alt
+    @logo_alt.to_s
   end
 
   def container_style

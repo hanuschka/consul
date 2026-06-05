bc22b4903e45f718a1f1be3df96e0f9bb194b0e5
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-06-05 18:27:50 +0300

    con-2818

--- app/helpers/adm/sections_helper.rb
+++ app/helpers/adm/sections_helper.rb
@@ -2,15 +2,17 @@ module Adm::SectionsHelper
   # Static view data for admin sections: icon + root path per section key.
   # Visibility is decided separately by Adm::SectionVisibility.
   def adm_sections
-    {
-      "administration"     => { icon: "admin_panel_settings",   path: adm_root_path },
-      "projekts"           => { icon: "folder",                 path: adm_projekts_root_path },
-      "landing_pages"      => { icon: "web",                    path: adm_landing_pages_root_path },
-      "moderation"         => { icon: "shield",                 path: adm_moderation_root_path },
-      "deficiency_reports" => { icon: "report_problem",         path: adm_deficiency_reports_root_path },
-      "ideas"              => { icon: "lightbulb",              path: adm_ideas_root_path },
-      "valuation"          => { icon: "account_balance_wallet", path: adm_valuation_root_path },
-      "officing"           => { icon: "how_to_vote",            path: adm_officing_root_path }
+    paths = {
+      "administration" => adm_root_path,
+      "projekts" => adm_projekts_root_path,
+      "landing_pages" => adm_landing_pages_root_path,
+      "moderation" => adm_moderation_root_path,
+      "deficiency_reports" => adm_deficiency_reports_root_path,
+      "ideas" => adm_ideas_root_path,
+      "valuation" => adm_valuation_root_path,
+      "officing" => adm_officing_root_path
     }
+
+    paths.to_h { |key, path| [key, { icon: Adm::Section::ICONS[key], path: path }] }
   end
 end
--- app/models/adm/section.rb
+++ app/models/adm/section.rb
@@ -2,6 +2,17 @@ module Adm
   module Section
     NAMES = %w[ideas deficiency_reports projekts moderation valuation landing_pages officing].freeze
 
+    ICONS = {
+      "administration" => "admin_panel_settings",
+      "projekts" => "folder",
+      "landing_pages" => "web",
+      "moderation" => "shield",
+      "deficiency_reports" => "report_problem",
+      "ideas" => "lightbulb",
+      "valuation" => "account_balance_wallet",
+      "officing" => "how_to_vote"
+    }.freeze
+
     MANAGER_PREDICATES = {
       "projekts"           => :projekt_manager?,
       "ideas"              => :idea_manager?,

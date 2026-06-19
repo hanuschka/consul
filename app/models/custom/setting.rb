89efdc9e9bef8501ec3d7956caeda91a37cfaf67
Michael Svystun <michael.svystun@gmail.com>
2026-06-19 10:03:04 +0100

    minor fix

--- app/models/custom/setting.rb
+++ app/models/custom/setting.rb
@@ -229,7 +229,6 @@ class Setting < ApplicationRecord
 
         "deficiency_reports.admins_must_assign_officer": false,
         "deficiency_reports.officers_can_administer_assigned_reports": true,
-        "deficiency_reports.officers_can_edit_assigned_reports": false,
         "deficiency_reports.allow_voting": false,
         "deficiency_reports.enable_comments": true,
         "deficiency_reports.intro_text": false,

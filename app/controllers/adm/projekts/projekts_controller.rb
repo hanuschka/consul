1646ec3da594f28dd0d39f586108c07595f8b7e0
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-07-15 12:40:53 +0300

    Fix

--- app/controllers/adm/projekts/projekts_controller.rb
+++ app/controllers/adm/projekts/projekts_controller.rb
@@ -1,7 +1,7 @@
 class Adm::Projekts::ProjektsController < Adm::Projekts::BaseController
   PROJEKT_IMAGE_MAX_FILE_SIZE_MB = 40
 
-  before_action :find_projekt, only: [:details, :visibility, :projekt_managers, :map, :phases, :images, :documents, :evaluation, :report_summary, :evaluation_phase, :poll_answer_participation, :poll_answer_crossectional, :evaluation_visibility, :update_evaluation_visibility, :toggle_evaluation_section_visibility, :generate_evaluation, :evaluation_status, :regenerate_phase_evaluation, :phase_evaluation_status, :evaluation_pdf_options, :evaluation_pdf, :update, :destroy, :toggle_activated, :update_default_phase, :notify_reviewers, :toggle_hide_content_background, :convert_to_new_content_block_mode, :update_color, :update_taxonomy, :update_image, :delete_image, :generate_image, :generate_image_status]
+  before_action :find_projekt, only: [:details, :visibility, :projekt_managers, :map, :phases, :images, :documents, :evaluation, :report_summary, :evaluation_phase, :poll_answer_participation, :poll_answer_crossectional, :evaluation_visibility, :update_evaluation_visibility, :toggle_evaluation_section_visibility, :generate_evaluation, :evaluation_status, :regenerate_phase_evaluation, :regenerate_phase_regular_stats, :regenerate_phase_ai_stats, :phase_evaluation_status, :evaluation_pdf_options, :evaluation_pdf, :update, :destroy, :toggle_activated, :update_default_phase, :notify_reviewers, :toggle_hide_content_background, :convert_to_new_content_block_mode, :update_color, :update_taxonomy, :update_image, :delete_image, :generate_image, :generate_image_status]
   before_action :set_back_button_url, only: [:details, :visibility, :projekt_managers, :map, :phases, :images, :documents, :evaluation]
   before_action :process_tags, only: [:update]
 
@@ -170,6 +170,15 @@ class Adm::Projekts::ProjektsController < Adm::Projekts::BaseController
     @active_phase_id = params[:phase_id].to_i
     @projekt_phase = @projekt.projekt_phases.find_by(id: @active_phase_id)
     @selection = PdfServices::EvaluationPdfSelection.all(@evaluation)
+
+    @poll_stats_entries =
+      if @projekt_phase.is_a?(ProjektPhase::VotingPhase)
+        @projekt_phase.polls.order(:id).map do |poll|
+          { poll: poll, stats: Poll::Stats.new(poll) }
+        end
+      else
+        []
+      end
 
     @back_button_url = evaluation_adm_projekts_projekt_path(@projekt)
 

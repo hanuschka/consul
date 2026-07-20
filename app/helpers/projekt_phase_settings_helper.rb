13368664e6f37f60bfb4968f64900cf6073d1b43
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-07-20 19:40:35 +0300

    Fix

--- app/helpers/projekt_phase_settings_helper.rb
+++ app/helpers/projekt_phase_settings_helper.rb
@@ -82,9 +82,15 @@ module ProjektPhaseSettingsHelper
 
   def footer_evaluation_tab_disabled?(projekt_phase, tab)
     return false if tab != "ai"
-    return false if footer_phase_evaluation_completed?(projekt_phase)
+    return false if Ai::Settings.ai_available?
 
-    !Ai::Settings.ai_available?
+    !footer_frozen_ai_content?(projekt_phase)
+  end
+
+  def footer_frozen_ai_content?(projekt_phase)
+    return false if !footer_phase_evaluation_completed?(projekt_phase)
+
+    projekt_phase.projekt_phase_evaluation&.ai_content? || false
   end
 
   def footer_admin_or_projekt_manager?

f576879d06c950ec91a5324ef6a21c306f6bd89b
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-07-29 13:32:42 +0300

    Update

--- app/controllers/adm/projekts/projekts_controller.rb
+++ app/controllers/adm/projekts/projekts_controller.rb
@@ -1,6 +1,21 @@
 class Adm::Projekts::ProjektsController < Adm::Projekts::BaseController
   PROJEKT_IMAGE_MAX_FILE_SIZE_MB = 40
 
+  # Switches on the visibility page, in display order. The first six are
+  # `projekts` columns (see Projekt::KEY_TO_COLUMN); the rest are
+  # projekt_settings rows.
+  VISIBILITY_SETTINGS = %i[
+    show_in_overview_page
+    show_in_overview_page_navigation
+    show_in_homepage
+    show_in_navigation
+    show_in_sidebar_filter
+    show_in_individual_list
+    show_related_projekt_link
+    allow_indexing
+    consider_underway
+  ].freeze
+
   before_action :find_projekt, only: [:details, :visibility, :projekt_managers, :map, :phases, :images, :documents, :evaluation, :report_summary, :evaluation_phase, :poll_answer_participation, :poll_answer_crossectional, :evaluation_visibility, :update_evaluation_visibility, :toggle_evaluation_section_visibility, :toggle_evaluation_tab_visibility, :generate_evaluation, :evaluation_status, :regenerate_phase_evaluation, :regenerate_phase_regular_stats, :regenerate_phase_ai_stats, :phase_evaluation_status, :evaluation_pdf_options, :evaluation_pdf, :update, :destroy, :toggle_activated, :update_default_phase, :notify_reviewers, :toggle_hide_content_background, :convert_to_new_content_block_mode, :update_color, :update_taxonomy, :update_image, :delete_image, :generate_image, :generate_image_status]
   before_action :set_back_button_url, only: [:details, :visibility, :projekt_managers, :map, :phases, :images, :documents, :evaluation]
   before_action :process_tags, only: [:update]
@@ -509,9 +524,7 @@ class Adm::Projekts::ProjektsController < Adm::Projekts::BaseController
   def toggle_activated
     authorize [:adm, :projekts, @projekt], :update?
 
-    setting = @projekt.projekt_settings.find_by(key: "projekt_feature.main.activate")
-    new_value = ActiveModel::Type::Boolean.new.cast(params[:projekt][:activated]) ? "active" : ""
-    setting.update!(value: new_value)
+    @projekt.update!(activated: ActiveModel::Type::Boolean.new.cast(params[:projekt][:activated]))
   end
 
   def update_default_phase

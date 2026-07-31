b81f1a6e8f3ebafa33bfde04d5d073004fe34127
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-07-31 22:05:56 +0300

    Update

--- app/helpers/adm/features_helper.rb
+++
@@ -1,8 +0,0 @@
-module Adm::FeaturesHelper
-  def kobil_credential_badge_style(configured:, required:)
-    return "success" if configured
-    return "danger" if required
-
-    "warning"
-  end
-end

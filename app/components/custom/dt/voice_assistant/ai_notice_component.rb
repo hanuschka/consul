97610d05e975b39ac604841a183e8b105416dd47
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-08-25 16:32:43 +0300

    Fix

--- app/components/custom/dt/voice_assistant/ai_notice_component.rb
+++ app/components/custom/dt/voice_assistant/ai_notice_component.rb
@@ -2,4 +2,12 @@ class Dt::VoiceAssistant::AiNoticeComponent < ApplicationComponent
   def notice_text
     I18n.t("custom.voice_assistant.ai_notice")
   end
+
+  def details_label
+    I18n.t("custom.voice_assistant.ai_notice_details_label")
+  end
+
+  def details
+    @details ||= I18n.t("custom.voice_assistant.ai_notice_details")
+  end
 end

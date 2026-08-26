48953600154e63f21dfea1ec58bdd8d5de8052e0
Michael Svystun <michael.svystun@gmail.com>
2026-08-26 09:42:27 +0100

    CON-2853

--- lib/remote_translations/caller.rb
+++ lib/remote_translations/caller.rb
@@ -1,45 +1,56 @@
 class RemoteTranslations::Caller
   attr_reader :remote_translation
 
-  def initialize(remote_translation, source_locale: nil)
+  def initialize(remote_translation, source_locale: nil, backfill: false)
     @remote_translation = remote_translation
     @source_locale = source_locale
+    @backfill = backfill
   end
 
   def call
-    update_resource
-    destroy_remote_translation
+    written = ActiveRecord::Base.no_touching { write_translation }
+
+    resource.touch if written && !backfill?
+    remote_translation.destroy
+  rescue StandardError => e
+    remote_translation.update(error_message: "#{e.class}: #{e.message}")
   end
 
   private
 
-    def update_resource
-      Globalize.with_locale(locale) do
-        resource.translated_attribute_names.each_with_index do |field, index|
-          resource.send(:"#{field}=", translations[index])
-        end
-      end
-      resource.save
+    def backfill?
+      @backfill
     end
 
-    def destroy_remote_translation
-      if resource.valid?
-        remote_translation.destroy
-        resource.save!
-      else
-        remote_translation.update(error_message: resource.errors.messages)
+    def write_translation
+      row = existing_row
+      return false if row&.hidden_at.present?
+
+      row ||= resource.translations.new(locale: locale)
+
+      resource.translated_attribute_names.each_with_index do |field, index|
+        row[field] = translated_values[index]
       end
+
+      return false unless row.changed?
+
+      row.save!
+      true
+    end
+
+    def existing_row
+      resource.translations.with_deleted.find_by(locale: locale)
     end
 
     def resource
       @resource ||= remote_translation.remote_translatable
     end
 
-    def translations
-      @translations ||= Deepl::Client.new.translate(fields_values,
-                                                    target_locale: locale,
-                                                    source_locale: deepl_source_locale,
-                                                    tag_handling: "html")
+    def translated_values
+      @translated_values ||= Deepl::Client.new.translate(fields_values,
+                                                         target_locale: locale,
+                                                         source_locale: deepl_source_locale,
+                                                         tag_handling: "html")
     end
 
     def fields_values

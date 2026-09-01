84e99d205c3c9571cffa1fb8c5e12eed38bac672
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2026-09-01 17:26:00 +0300

    CON-3012

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -76,6 +76,16 @@ class ProposalsController
       take_by_projekts(@scoped_projekt_ids)
     end
 
+    if request.format.json?
+      render json: JSON.generate(
+        MapLocation.flatten_feature_collections(
+          all_proposal_map_locations(@resources)
+        )
+      )
+
+      return
+    end
+
     @proposals_map_pin_count =
       proposal_map_pin_count_up_to(@resources, MAP_PINS_LAZY_LOAD_THRESHOLD)
 
@@ -95,14 +105,6 @@ class ProposalsController
         else
           render :index
         end
-      end
-
-      format.json do
-        render json: JSON.generate(
-          MapLocation.flatten_feature_collections(
-            all_proposal_map_locations(@resources)
-          )
-        )
       end
 
       format.csv do

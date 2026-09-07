204a21691e3ccb1c2932a20b8465964ec765786f
Michael Svystun <michael.svystun@gmail.com>
2026-09-07 11:11:34 +0100

    sync

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -3,7 +3,7 @@ require_relative "../lib/middleware/downcase_route"
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :de
-    config.i18n.available_locales = [:de, :en]
+    config.i18n.available_locales = [:de, :en, :fr, :es, :pt, :nl, :it, :pl, :ru, :"zh-CN"]
     config.active_model.i18n_customize_full_message = true
 
     config.middleware.insert_before(0, DowncaseRoute)
